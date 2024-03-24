6a0bbd3bd5e7e42032dc70e17b515effece2825c
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-03-24 12:58:20 -0500

    topics-funtion

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,8 +2,9 @@ import './style.css'
 
 // import './topics/01-basic-types.ts'
 // import './topics/02-object-interface.ts'
+// import './topics/03-basic-funtions.ts'
 
-import './topics/03-basic-funtions.ts'
+import './topics/04-homework-types.ts'
 
 import { setupCounter } from './counter.ts'
 
--- 01-typescript-intro/src/topics/03-basic-funtions.ts
+++ 01-typescript-intro/src/topics/03-basic-funtions.ts
@@ -14,13 +14,49 @@ function multiply (firstNumber: number, secondNumber?: number, base : number = 3
     return firstNumber * base;
 }
 
-let result = sumNumbers(2,5)
-const resultArrow = sumNumbersArrow(4,3)
-let multiplyResult = multiply(3)
+// let result = sumNumbers(2,5)
+// const resultArrow = sumNumbersArrow(4,3)
+// let multiplyResult = multiply(3)
 
 
-console.log(result, resultArrow, multiplyResult)
+// console.log(result, resultArrow, multiplyResult) 
 
+
+
+interface personaje {
+    name: string;
+    hp: number;
+    showHp: () => void
+}
+
+function helpPersonaje (personaje:personaje, amount: number) {
+
+    if (personaje.hp >= 100){
+        console.log("FULL")
+    }else{
+        personaje.hp += amount
+    }
+
+
+
+}
+
+
+// No se usa la funcion fecha debido a que ya se especificó
+
+const pikachu:personaje = {
+    name : "pikachu",
+    hp : 30,
+    showHp () {
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+}
+pikachu.showHp();
+
+helpPersonaje(pikachu, 15);
+helpPersonaje(pikachu, 11);
+
+pikachu.showHp();
 
 
 

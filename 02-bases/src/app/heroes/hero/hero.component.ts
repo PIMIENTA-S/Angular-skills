04ddf99c3464e30b90e634f36bf41d7c03c1bc6f
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-07 22:27:41 -0500

    Fix(Hero-list)

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+
+  public name: string = 'Iroman';
+  public age: number = 45;
+
+  // los get en angular son igaules a las propiedades
+
+  get capitalizado():string{
+    return this.name.toUpperCase();
+  }
+
+  getDescriptionHero():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero():void {
+    this.name = 'Spiderman'
+
+    // cuando no hayamos implementado el metodo podemos usar el throw
+    // con esto mandaremos un string y evitar que colapse la app
+    // throw 'Metodo por implementar'
+  }
+
+  changeAge(){
+    this.age = 25
+
+  }
 
 }

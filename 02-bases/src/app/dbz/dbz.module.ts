0b591a2d02ee922cd1be278b69c21e4a762725e2
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-12 10:30:51 -0500

    feat(refact)

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { mainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';

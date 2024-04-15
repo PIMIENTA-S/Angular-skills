e5666c119cc6a476f257ff44e4d97fdc64a08254
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-14 23:26:58 -0500

    feat(btn-onDelete)

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Characters } from './../../interfaces/characters.interface';
 
@@ -11,11 +11,19 @@ import { Characters } from './../../interfaces/characters.interface';
 export class ListComponent {
 
   @Input()
-
   public charactersList: Characters[] = [{
     name: 'Picoro',
     power: 7000
-  }]
+  }];
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+
+  onDeleteCharacter(index: number):void{
+    let i = index
+    console.log(index)
+    this.onDelete.emit(i)
+  };
   
   // hola(){
   //   console.log(this.charactersList)

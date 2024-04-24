00e33bd2f85a413e250f7c8c078c486c125a5697
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-23 22:17:49 -0500

    debugging

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -11,9 +11,26 @@ export class GifsService {
         return [...this._SearchHistory]
     }
 
+    orderTag( tag: string ){
+
+        tag = tag.toLowerCase()
+
+        for (let i = 0; i <= this._SearchHistory.length; i ++ ){
+
+            if (this._SearchHistory[i] === tag){
+                delete this._SearchHistory[i]
+
+            }
+
+        }
+
+    }
+
     searchTag( tag: string ): void{
 
         if (tag.length === 0){return;}
+
+        this.orderTag(tag)
 
         this._SearchHistory.unshift(tag)
         

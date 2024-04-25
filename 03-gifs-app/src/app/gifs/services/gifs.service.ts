78720c8e9a30808f5b603fdc4693006134265207
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-24 23:19:12 -0500

    testing api

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,39 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
     
-    private _SearchHistory: string[] = []
+    private _SearchHistory: string[] = [];
+    private api_key : string = 'vacJu6iaVCJDQTiup44maLYMsXJEU6pM'
+    private urlApi  : string = 'https://api.giphy.com/v1/gifs/'
     
-    constructor() { }
+    constructor( private http: HttpClient ) { }
 
     get tagHistory(){
         return [...this._SearchHistory]
     }
 
-    orderTag( tag: string ){
+    orderTag( list: string[] ){
 
-        tag = tag.toLowerCase()
+        // tag = tag.toLowerCase()
 
-        for (let i = 0; i <= this._SearchHistory.length; i ++ ){
+        // for (let i = 0; i <= this._SearchHistory.length; i ++ ){
 
-            if (this._SearchHistory[i] === tag){
-                delete this._SearchHistory[i]
+        //     if (this._SearchHistory[i] === tag){
+        //         delete this._SearchHistory[i]
 
-            }
+        //     }
 
-        }
+        // }
+
+        const array = new Set (list.splice(0,10));
+        this._SearchHistory = [...array]
+
+
 
     }
 
     searchTag( tag: string ): void{
 
         if (tag.length === 0){return;}
-
-        this.orderTag(tag)
-
-        this._SearchHistory.unshift(tag)
         
+        this._SearchHistory.unshift(tag.toLowerCase())
+        
+        this.orderTag(this._SearchHistory)
+
+        const params = new HttpParams() 
+            .set('api_key', this.api_key)
+            .set('limit', '15')
+            .set('q', tag)
+
+        this.http.get(` ${ this.urlApi }search`, { params })
+            .subscribe(resp => {
+                console.log(resp)
+            })
+
+
+
+        // this.http.get('https://api.giphy.com/v1/gifs/search?api_key=vacJu6iaVCJDQTiup44maLYMsXJEU6pM&q=valorant&limit=15')
+        // .subscribe(resp => {
+        //     console.log(resp)
+        // })
     }
 
 

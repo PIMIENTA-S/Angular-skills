589580f5edbc89f3dbdac17a73152f071c0abd51
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-04-28 22:48:02 -0500

    feat(card-list)

---
+++ 03-gifs-app/src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+import { Gif } from './../../interfaces/gifs.interface';
+
+
+@Component({
+    selector: 'gifs-card',
+    templateUrl: 'gifs-card.component.html'
+})
+
+export class GifsCardComponent  {
+    
+    @Input()
+    public receiveGifs!: Gif;
+    
+    constructor() { }
+
+    
+}

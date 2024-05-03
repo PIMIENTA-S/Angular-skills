59c5d6d581fbef6c792dccb7d87591bcaaa633a5
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-05-02 21:19:38 -0500

    feat(routing)

--- 04-country-app/src/app/app-routing.module.ts
+++ 04-country-app/src/app/app-routing.module.ts
@@ -1,28 +1,42 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 
 const routes : Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
   },
   {
     path: 'about',
     component: AboutPageComponent,
   },
+
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+
   {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   }
 
 
 ]
-
+// se importa con .forRoot debido a que es el router principal de la pagina
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  // exports: [RouterModule]
+
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+
+  exports: [
+    RouterModule
+  ]
+
 })
 export class AppRoutingModule { }
